144c0e984b06d822a2a822ffc51e67afd07e46a8
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-12-30 20:41:15 -0300

    create home

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Animated,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -14,8 +15,12 @@ import {
 import WelcomeUser from "../../components/WelcomeUser";
 import Colors from "../../constants/Colors";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useRef } from "react";
 const ShoppingListCard = () => {
-  const iconColor = useThemeColor({ light: "black", dark: "white" }, "text");
+  const iconColor = useThemeColor(
+    { light: Colors.dark.background, dark: Colors.light.background },
+    "text"
+  );
   return (
     <ThemedView
       style={{
@@ -53,59 +58,131 @@ const ShoppingListCard = () => {
             <Text style={{ fontSize: 10 }}>34</Text>
             <Text style={{ opacity: 0.5, fontSize: 10 }}>/56</Text>
           </View>
-          <FontAwesome name="angle-right" size={16} color={iconColor} />
+          <FontAwesome
+            name="angle-right"
+            size={20}
+            color={iconColor}
+            style={{ marginLeft: 8 }}
+          />
         </TouchableOpacity>
       </View>
     </ThemedView>
   );
 };
 const Home = () => {
+  const contentColor = useThemeColor(
+    { light: "white", dark: "black" },
+    "background"
+  );
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  const translateHeader = scrollY.interpolate({
+    inputRange: [0, 170],
+    outputRange: [0, -170],
+    extrapolate: "clamp",
+  });
+
+  const translateTitle = scrollY.interpolate({
+    inputRange: [0, 280],
+    outputRange: [0, 90],
+    extrapolate: "clamp",
+  });
+
+  const opacityTitle = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [1, 0],
+    extrapolate: "clamp",
+  });
+
+  const translateMenu = scrollY.interpolate({
+    inputRange: [0, 170],
+    outputRange: [0, 170],
+    extrapolate: "clamp",
+  });
+
   return (
     <SafeScreen
       darkColor={Colors.dark.mainColor}
       lightColor={Colors.light.mainColor}
     >
-      <ThemedView
-        style={styles.headerContainer}
-        darkColor={Colors.dark.mainColor}
-        lightColor={Colors.light.mainColor}
+      <Animated.View
+        style={[
+          styles.headerContainer,
+          { transform: [{ translateY: translateHeader }] },
+        ]}
       >
-        <View style={styles.menu}>
-          <TouchableOpacity></TouchableOpacity>
-          <TouchableOpacity></TouchableOpacity>
-        </View>
-        <View>
-          <WelcomeUser name="Genilson" />
+        <ThemedView
+          style={styles.headerContent}
+          darkColor={Colors.dark.mainColor}
+          lightColor={Colors.light.mainColor}
+        >
+          <Animated.View style={{ transform: [{ translateY: translateMenu }] }}>
+            <View style={styles.menu}>
+              <TouchableOpacity>
+                <FontAwesome name="search" size={20} color={"white"} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <FontAwesome name="cog" size={20} color={"white"} />
+              </TouchableOpacity>
+            </View>
+          </Animated.View>
+          <View>
+            <Animated.View
+              style={[
+                {
+                  transform: [{ translateY: translateTitle }],
+                  opacity: opacityTitle,
+                },
+              ]}
+            >
+              <WelcomeUser name="Genilson" />
+            </Animated.View>
 
-          <ThemedView
-            style={{
-              paddingHorizontal: 24,
-              paddingVertical: 16,
-              borderTopEndRadius: 30,
-              borderTopStartRadius: 30,
-            }}
-          >
-            <Text style={{ fontSize: 16 }}>Lista de compras</Text>
-          </ThemedView>
-        </View>
-      </ThemedView>
-      <ScrollView>
-        <ThemedView style={styles.contentContainer}>
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
-          <ShoppingListCard />
+            <ThemedView
+              style={{
+                paddingHorizontal: 24,
+                paddingVertical: 16,
+                borderTopEndRadius: 30,
+                borderTopStartRadius: 30,
+                marginTop: -2,
+              }}
+            >
+              <Text style={{ fontSize: 16 }}>Lista de compras</Text>
+            </ThemedView>
+          </View>
+        </ThemedView>
+      </Animated.View>
+      <Animated.ScrollView
+        contentContainerStyle={styles.contentContainer}
+        style={{
+          backgroundColor: contentColor,
+        }}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          {
+            useNativeDriver: true,
+          }
+        )}
+        scrollEventThrottle={1}
+      >
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+        <ShoppingListCard />
+      </Animated.ScrollView>
+      <ThemedView style={{ paddingHorizontal: 24, paddingVertical: 16 }}>
+        <TouchableOpacity>
           <ThemedView
             darkColor={Colors.dark.mainColor}
             lightColor={Colors.light.mainColor}
@@ -116,12 +193,15 @@ const Home = () => {
               justifyContent: "center",
             }}
           >
-            <Text style={{ fontSize: 14, textAlign: "center" }}>
+            <Text
+              lightColor={Colors.dark.text}
+              style={{ fontSize: 14, textAlign: "center" }}
+            >
               Criar Nova lista
             </Text>
           </ThemedView>
-        </ThemedView>
-      </ScrollView>
+        </TouchableOpacity>
+      </ThemedView>
     </SafeScreen>
   );
 };
@@ -130,7 +210,6 @@ const styles = StyleSheet.create({
   headerContainer: {
     height: 280,
     width: "100%",
-    justifyContent: "space-between",
     position: "absolute",
     left: 0,
     top: 0,
@@ -145,10 +224,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     paddingVertical: 16,
   },
+  headerContent: {
+    width: "100%",
+    height: 280,
+    justifyContent: "space-between",
+  },
   contentContainer: {
-    flex: 1,
     paddingHorizontal: 24,
-    paddingTop: 280 + 16,
+    paddingTop: 280 + 14,
   },
 });
 
